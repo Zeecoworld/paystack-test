668a1a405b55d6f4a3bcec4934d3d2723b7935e4
ISAACGEEK <engrisaac1234@gmail.com>
2023-03-09 19:10:28 +0100

    added db from railway

--- paystackpay/settings.py
+++ paystackpay/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'paystackpay.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway', 
+        'USER': 'postgres',
+        'PASSWORD': 'kAtxKMkw2tze3k2eKHDq',
+        'HOST': 'containers-us-west-17.railway.app', 
+        'PORT': '6824',
     }
 }
 
